a7243eb5c5b2319211df69289e3959b0148cd418
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-12-21 16:41:53 +0530

    Change Header to Stateful

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,45 @@
 import { Link } from "gatsby"
-import * as React from "react"
 import logo from "../images/logo-designcode.svg"
 import "./Header.css"
 
-const Header = ({ siteTitle }) => (
- <div className="Header">
-  <div className="HeaderGroup">
-  <Link to="/">
-    <img src={logo} width="30" alt="logo"/>
-  </Link>
-  <Link to="/courses">Courses</Link>
-  <Link to="/downloads">Downloads</Link>
-  <Link to="/workshops">Workshops</Link>
-  <Link to="/buy"><button>Buy</button></Link>
-  </div>
- </div>
-)
+import React, { Component } from 'react'
 
-export default Header
+export default class Header extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      hasScrolled:false
+    }
+  }
+  componentDidMount(){
+    window.addEventListener('scroll',this.handleScroll)
+  }
+
+  handleScroll = (event)=>{
+    const scrollTop = window.scrollY
+    if(scrollTop > 50){
+      this.setState({hasScrolled: true})
+    }
+    else{
+      this.setState({hasScrolled:false})
+    }
+  }
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+      <div className="HeaderGroup">
+      <Link to="/">
+        <img src={logo} width="30" alt="logo"/>
+      </Link>
+      <Link to="/courses">Courses</Link>
+      <Link to="/downloads">Downloads</Link>
+      <Link to="/workshops">Workshops</Link>
+      <Link to="/buy"><button>Buy</button></Link>
+      </div>
+     </div>
+    )
+  }
+}
+
+
