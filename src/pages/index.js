417bde1f9ece6b3c76d96b4612849eb8112149db
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-12-22 17:12:43 +0530

    Styled cells

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
+import styled from "styled-components"
 
 // import Logos
 import SketchLogo from "../images/Logos/logo-sketch.png"
@@ -23,6 +24,7 @@ import Card from "../components/Card"
 import Section from "../components/Section"
 import Wave from "../components/Wave"
 import staticData from "../../staticData.json"
+import Cell from "../components/Cell"
 
 const samplePageLinks = [
   {
@@ -33,6 +35,25 @@ const samplePageLinks = [
       "A simple example of linking to another page within a Gatsby site",
   },
 ]
+
+const SectionCaption = styled.p`
+font-weisght:600;
+font-size:18px;
+text-transform:uppercase;
+color:#94A4BA;
+text-align:center;`
+
+const SectionCellGroup = styled.div`
+max-width:800px;
+margin:0 auto 100px;
+display:grid;
+grid-template-columns:repeat(2,1fr);
+column-gap:20px;
+padding:0 20px;
+
+@media(max-width:800px){
+  grid-template-columns:repeat(1,1fr);
+}`
 
 const IndexPage = () => (
   <Layout>
@@ -61,14 +82,18 @@ const IndexPage = () => (
             <img src={ReactLogo} width="50" alt="react-logo" />
             <img src={SwiftLogo} width="50" alt="swift-logo" />
           </div>
-          <Wave/> 
+          <Wave />
         </div>
       </div>
       <div className="Cards">
         <h2>11 Courses, more coming</h2>
         <div className="CardGroup">
           <Card title="Design Systemn" text="10 Sections" image={Wallpaper} />
-          <Card title="React for Designers" text="12 Sections" image={Wallpaper2} /> 
+          <Card
+            title="React for Designers"
+            text="12 Sections"
+            image={Wallpaper2}
+          />
           <Card title="sound Design" text="5 Sections" image={Wallpaper3} />
           <Card title="ARKit 2" text="10 Sections" image={Wallpaper4} />
         </div>
@@ -80,10 +105,13 @@ const IndexPage = () => (
         text="Learn how to build a modern site using React and the most efficient libraries to get yout site/product online. 
         Get familiar with components, Grid CSS, animations, interactions, dynamic data with Comntenful and deployinh your site with Netlify."
       />
-      {staticData.cells.map(cell=>(
-        <div>{cell.title}</div>
-      ))}
-      
+
+      <SectionCaption>12 Sections, 6 hours</SectionCaption>
+      <SectionCellGroup>
+        {staticData.cells.map(cell => (
+          <Cell title={cell.title} image={cell.image} />
+        ))}
+      </SectionCellGroup>
     </div>
   </Layout>
 )
